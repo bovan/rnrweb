c8d7576516dda44ae0735f3250404adf1ad18a92
Børge Antonsen <bovan79@gmail.com>
2014-10-14 19:46:38 +0200

    cherry picked some boilerplate css etc

--- config/prod.php
+++ config/prod.php
@@ -1,6 +1,7 @@
 <?php
 
 // configure your app for the production environment
-
+$app['cache.path'] = __DIR__ . '/../cache';
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+
